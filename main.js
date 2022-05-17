3bd210e88f8229d79de9583c7c792bc615019593
arun9718 <arunavasahachowdhury@gmail.com>
2022-05-17 17:23:06 +0530

    Pseudo implementation

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
 let inputArr=process.argv.slice(2);
 console.log(inputArr);
+
+//tasks needed
+//1. tree command
+//2. organize command
+//3.help command
+
+let command=inputArr[0];
+
+switch(command){
+  case "tree":
+    break;
+  case "organize":
+    break;
+  case "help":
+    break;
+  default:
+    console.log("Please 🙏 input valid command!");
+
+}
+
+function TreeFn(dirPath){
+  console.log("Tree command implemented");
+}
+function OrganizeFn(dirPath){
+  console.log("Organize command implemented");
+}
+function helpFn(dirPath){
+  console.log("Help Command implemented");
+}
