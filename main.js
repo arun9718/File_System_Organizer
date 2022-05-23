bc9e6886a705d082c1d04533c005ec5b7c61ddc8
arun9718 <arunavasahachowdhury@gmail.com>
2022-05-23 13:35:52 +0530

    todays first commit

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let inputArr=process.argv.slice(2);
-console.log(inputArr);
+let fs = require('fs');
+let path = require('path');
 
 //tasks needed
 //1. tree command
@@ -10,10 +11,13 @@ let command=inputArr[0];
 
 switch(command){
   case "tree":
+    TreeFn(inputArr[1]);
     break;
   case "organize":
+    OrganizeFn(inputArr[1]);
     break;
   case "help":
+    helpFn();
     break;
   default:
     console.log("Please 🙏 input valid command!");
@@ -24,8 +28,33 @@ function TreeFn(dirPath){
   console.log("Tree command implemented");
 }
 function OrganizeFn(dirPath){
-  console.log("Organize command implemented");
+//  console.log("Organize command implemented");
+
+  if(dirPath==undefined){
+    console.log("Kindly enter correct path!");
+    return;
+  }
+  else{
+    let doesExist=fs.existsSync(dirPath);
+    if(doesExist){
+      let destPath=path.join(dirPath,"organized_files");
+      if(fs.existsSync(destPath)==false)
+        fs.mkdirSync(destPath);
+    }
+    else{
+      console.log("Kindly enter correct path");
+      return;
+    }
+
+  }
+
+
+
 }
-function helpFn(dirPath){
-  console.log("Help Command implemented");
+function helpFn(){
+  console.log(`
+  List of all the commands :-
+  1. tree command <dir path>
+  2. organize command <dir path>
+  3. help command`);
 }
