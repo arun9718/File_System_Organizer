333a3e0ede70d65a27014ae353cec70184e48060
arun9718 <arunavasahachowdhury@gmail.com>
2022-05-23 16:08:23 +0530

    Divided in modules

---
+++ commands/help.js
@@ -0,0 +1,7 @@
+function helpFn(){
+  console.log(`
+  List of all the commands :-
+  1. tree command <dir path>
+  2. organize command <dir path>
+  3. help command`);
+}
---
+++ commands/tree.js
@@ -0,0 +1,3 @@
+function TreeFn(dirPath){
+  console.log("Tree command implemented");
+}
